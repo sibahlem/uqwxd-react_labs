c92eb1fa08e7097f9f12c49cd78f2144d6e38d0b
Sibahle <60863779+sibahlem@users.noreply.github.com>
2024-03-19 02:52:53 +0200

    Update MainPanel.js

--- react-redux-master/src/components/MainPanel.js
+++ react-redux-master/src/components/MainPanel.js
@@ -1,3 +1,5 @@
+// MainPanel component which contains two internal Components, MyButton and DivPanel.
+
 import React from 'react'
 import MyButton from './MyButton'
 import DivPanel from './DivPanel';
@@ -11,3 +13,9 @@ const MainPanel = ()=>{
     );
 }
 export default MainPanel;
+
+// MyButton is a button component.
+// The application maintains a counter which keeps track of the number of times the button is clicked.
+// The value of this counter will be displayed in the DivPanel.
+// The content of DivPanel will be automatically refreshed everytime the counter value changes.
+// These are effectively two different components.
