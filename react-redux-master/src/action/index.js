22987522a3c997780e206f6ad275c9199c171f4d
Sibahle <60863779+sibahlem@users.noreply.github.com>
2024-03-19 02:48:05 +0200

    Update index.js

--- react-redux-master/src/action/index.js
+++ react-redux-master/src/action/index.js
@@ -1,3 +1,6 @@
+// val is the value you want to increase the counter by everytime the button is clicked.
+// Now that you have your action which defines what is to be done, you will create the reducers which will define how it is done.
+
 const increment = (val) => {
     return {
         type : 'INCREMENT',
