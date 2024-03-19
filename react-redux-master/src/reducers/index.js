323f24220f700f62bce3200748e9d85718b0081c
Sibahle <60863779+sibahlem@users.noreply.github.com>
2024-03-19 02:49:54 +0200

    Update index.js

--- react-redux-master/src/reducers/index.js
+++ react-redux-master/src/reducers/index.js
@@ -1,6 +1,6 @@
 import {combineReducers} from 'redux'
 
-const counter = (state=0, action) => {
+const counter = (state=0, action) => { // Remember reducer takes two parameters, previous app state and action. Then returns new app state.
     if (action.type === 'INCREMENT') {
         return state+action.inc; //This will increase the value of counter by the value passed to the increment method
     }
@@ -10,3 +10,6 @@ const counter = (state=0, action) => {
 const myReducers = combineReducers({counter});
 
 export default myReducers;
+
+// Now you have your action and reducers.
+// What is left to be created is the store. Before you create the store you will create the components.
